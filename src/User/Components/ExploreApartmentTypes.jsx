41d91abb42e2006e24ce41d162e348255a5baa68
rayhan <aburayhandh@gmail.com>
2023-12-01 18:53:05 +0600

    Explore apartments icons fixed

--- src/User/Components/ExploreApartmentTypes.jsx
+++ src/User/Components/ExploreApartmentTypes.jsx
@@ -9,11 +9,30 @@ const ExploreApartmentTypes = () => {
                 description='Get some Inspirations from 1800+ skills'
             />
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-7">
-                <ExploreApartmentTypesCard />
-                <ExploreApartmentTypesCard />
-                <ExploreApartmentTypesCard />
-                <ExploreApartmentTypesCard />
-                <ExploreApartmentTypesCard />
+                <ExploreApartmentTypesCard
+                    img='https://i.ibb.co/HN2QZz5/house.png'
+                    name='Houses'
+                />
+
+                <ExploreApartmentTypesCard
+                    img='https://i.ibb.co/X5dX77K/bulding.png'
+                    name='Appartments'
+                />
+
+                <ExploreApartmentTypesCard
+                    img='https://i.ibb.co/zmmvtDk/office-worker.png'
+                    name='Office'
+                />
+
+                <ExploreApartmentTypesCard
+                    img='https://i.ibb.co/swg0yzP/gardening.png'
+                    name='Villa'
+                />
+
+                <ExploreApartmentTypesCard
+                    img='https://i.ibb.co/X5dX77K/bulding.png'
+                    name='Townhome'
+                />
             </div>
         </div>
     );
--- src/User/Components/ExploreApartmentTypesCard.jsx
+++ src/User/Components/ExploreApartmentTypesCard.jsx
@@ -1,14 +1,14 @@
-const ExploreApartmentTypesCard = () => {
+const ExploreApartmentTypesCard = ({img, name}) => {
     return (
         <div className="bg-[#f7f7f7] p-5 rounded-lg hover:bg-black hover:text-white cursor-pointer">
             <div className="bg-white w-16 h-16 rounded-full flex justify-center items-center">
 
-                <img src="https://i.ibb.co/HN2QZz5/house.png" alt="" className="w-8" />
+                <img src={img} alt="" className="w-8" />
             </div>
 
             <div className="mt-20">
                 <p className="text-lg font-semibold">
-                    Houses
+                    {name}
                 </p>
 
                 <p className="text-sm">

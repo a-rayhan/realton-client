66c4665e6e4c7b5106e3f99db4d419f2201bf9f5
rayhan <aburayhandh@gmail.com>
2023-12-01 19:18:21 +0600

    Popular propertries card icons fixed

--- src/User/Components/PopulerPropertiesCard.jsx
+++ src/User/Components/PopulerPropertiesCard.jsx
@@ -3,6 +3,8 @@ import { FiExternalLink } from "react-icons/fi";
 import { IoBedOutline } from "react-icons/io5";
 import { MdOutlineLibraryAdd } from "react-icons/md";
 import { SlEnergy } from "react-icons/sl";
+import { LuBath } from "react-icons/lu";
+import { CiSquareMore } from "react-icons/ci";
 import { NavLink } from "react-router-dom";
 
 const PopulerPropertiesCard = () => {
@@ -48,7 +50,7 @@ const PopulerPropertiesCard = () => {
                     </div>
                     <div>
                         <div className="flex items-center gap-x-2">
-                            <IoBedOutline />
+                            <LuBath />
                             <p className="text-sm">
                                 1
                             </p>
@@ -59,7 +61,7 @@ const PopulerPropertiesCard = () => {
                     </div>
                     <div>
                         <div className="flex items-center gap-x-2">
-                            <IoBedOutline />
+                            <CiSquareMore />
                             <p className="text-sm">
                                 1200
                             </p>
